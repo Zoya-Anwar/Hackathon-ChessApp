c266dbc5b7aab8e54205ab8c6230cb342a18a6a9
Gwydion-Smit <gwydion.c.smit@gmail.com>
2021-02-27 20:16:05 +0000

    Co-authored-by: Aditya Unnithan <aditya.unnithan@student.manchester.ac.uk> Co-authored-by: Zoya-Anwar <Zoya-Anwar@users.noreply.github.com>

--- classes/Piece.cs
+++ classes/Piece.cs
@@ -7,6 +7,8 @@ public class Piece {
   	protected int abs_horizontal;
     protected int vertical;
     protected int horizontal;
+    protected int xcoord;
+    protected int ycoord;
 
     public Piece(PieceColour p)
     {
@@ -22,6 +24,13 @@ public class Piece {
      public PieceColour getColour(){
         return colour;
     }
+    
+    public void move(int newx, int newy)
+    {
+        xcoord = newx;
+        ycoord = newy;
+    }
+
     public virtual bool isLegitMove(int i0, int j0, int i1, int j1){
         abs_vertical = Math.Abs(i1 - i0); //get vert spaces moving
         abs_horizontal = Math.Abs(j1 - j0); //get horiz spaces moving
--- classes/Player.cs
+++ classes/Player.cs
@@ -1,30 +1,47 @@
-namespace Projects.Shared
-{
+using System.Collections.Generic;
+
     public class Player
     {
-        private Piece[] pieces;
+        private List<Piece> pieces;
         private PieceColour colour;
+        private bool lost = false;
 
-        public Player()
+        public Player(PieceColour c)
         {
-
+            colour = c;
         }
 
-        public void removePiece()
+        public void removePiece(Piece piece)
         {
-
+            pieces.RemoveAt(pieces.IndexOf(piece));
         }
 
-        public void movePiece()
+        public void movePiece(Piece piece, int newx, int newy)
         {
+            pieces[pieces.IndexOf(piece)].move(newx, newy);
+        }
 
+        public void addPiece(int startx, int starty, Piece piece)
+        {
+            piece.move(startx, starty);
+            pieces.Add(piece);
         }
 
         public Piece[] getPieces()
         {
-            return pieces;
+            return pieces.ToArray();
         }
 
-        
+        public Piece getKing()
+        {
+            foreach(Piece piece in pieces)
+            {
+                if(piece.GetType() == typeof(King))
+                {
+                    return piece;
+                }
+            }
+            return null;
+        }
+
     }
-}
